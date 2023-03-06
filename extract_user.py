773e3cadc5ed58fdf811a1ab1cafeb7742e3df4f
Jeremy MIKALEFF <jeremy.mikaleff@gmail.com>
2023-03-06 15:08:37 +0100

    reparation

--- extract_user.py
+++ extract_user.py
@@ -1,49 +1,87 @@
+import yaml
 import pymongo
-import mysql.connector
+from sqlmodel import create_engine, Session, SQLModel, Column, Relationship, ForeignKey, String, Integer, Field, Text
+from typing import List, Optional
+from datetime import date, datetime
 
-# Test de connexion à MongoDB
-try:
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["g2-MOOC"]
-    collection1 = db["user"]
-    collection2 = db["forum"]
-    print("Connexion à MongoDB réussie !")
-except pymongo.errors.ConnectionFailure as e:
-    print("Erreur de connexion à MongoDB :", e)
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
 
-# Test de connexion à MySQL
-try:
-    mysql_conn = mysql.connector.connect(
-      host="localhost",
-      user="root",
-      password="greta2023",
-      database="g2",
+host = config['database']['host']
+port = config['database']['port']
+user_connect = config['database']['user']
+password = config['database']['password']
+db_name = config['database']['db_name']
+# Créez une connexion à la base de données SQL et utilisez les instances de modèle pour insérer des données
+# Créez une instance de moteur de base de données en utilisant le pilote MySQL
+engine = create_engine(
+    f"mysql+pymysql://{user_connect}:{password}@{host}:{port}/{db_name}", echo=True)
+
+# Créez une connexion à la base de données MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["mooc"]
+collection = db["user"]
+
+
+class User(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    city: Optional[str] = None
+    country: Optional[str] = None
+    gender: Optional[str] = None
+    year_of_birth: Optional[date] = None
+
+
+# Liste des résultats
+users = []
+
+# Champs attendus pour chaque cours
+expected_fields = ['city', 'country', 'gender', 'year_of_birth']
+
+# Parcours des utilisateurs
+for doc in collection.find({}):
+    # Récupération de l'utilisateur
+    username = doc.get('username')
+    if not username:
+        continue
+
+    # Récupération des champs
+    fields = {}
+    for key, value in doc.items():
+        if key in expected_fields:
+            fields[key] = value
+
+    # Création d'un résultat
+    user = User(
+        username=username,
+        **fields
     )
-    print("Connexion à MySQL réussie !")
-except mysql.connector.Error as e:
-    print("Erreur de connexion à MySQL :", e)
 
-# Sélection des champs que vous voulez récupérer
-query = {}
-projection = {"username": 1, "id": 1, "year_of_birth" : 1 }
+    # Vérification de l'existence de l'utilisateur
+    with Session(engine) as session:
+        existing_user = session.query(User).filter(
+            User.username == username).first()
 
-#username = collection.distinct("username", "id")
+    if existing_user:
+        # Mise à jour de l'utilisateur existant avec les nouvelles données
+        existing_user.city = user.city
+        existing_user.country = user.country
+        existing_user.gender = user.gender
+        existing_user.year_of_birth = user.year_of_birth
+        user = existing_user
 
-# Récupération des données depuis MongoDB
-username = collection1.find(query, projection).limit(1000)
-'''for doc in username:
-    print (doc)'''
+    # Ajout du résultat à la liste
+    users.append(user)
 
-# Insertion des données dans la table MySQL
-count=0
-mysql_cursor = mysql_conn.cursor()
+# Créez une instance de métadonnées SQLModel
+metadata = SQLModel.metadata
 
-mysql_cursor.execute("CREATE TABLE IF NOT EXISTS User (user_id VARCHAR(255), username VARCHAR(255), year_of_birth VARCHAR(255))")
+metadata.create_all(engine)
 
-for d in username:
-    count=count+1
-    sql = "INSERT INTO g2.User (user_id, username, year_of_birth) VALUES (%s, %s, %s)"
-    values = (d["id"], d["username"], d["year_of_birth"])
-    print(f'{d["id"]} ----- {d["username"]} ----- {d["username"]} ----- {count}')
-    mysql_cursor.execute(sql, values)
-    mysql_conn.commit()
+# Utilisez la session pour interagir avec la base de données
+with Session(engine) as session:
+    # Insérez les threads dans la base de données
+    session.add_all(users)
+
+    # Validez les transactions et confirmez les modifications dans la base de données
+    session.commit()
